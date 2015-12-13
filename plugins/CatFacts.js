b0cba821f090619e54b1e23adac24af42accaca0
twof <fabiobean2@gmail.com>
2015-12-13 15:08:33 -0800

    Fixed errors. #thankDavid

--- plugins/CatFacts.js
+++ plugins/CatFacts.js
@@ -2,9 +2,15 @@ module.exports = {listeners: [
 {
 	type: "equals",
 	query: "CF?",
+  function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+  }
   callback: function(reply, message){
-    var json = http://catfacts-api.appspot.com/api/facts?number=1;
-    obj = JSON.parse(json);
+    var json = "http://catfacts-api.appspot.com/api/facts?number=1";
+    obj = JSON.parse(Get(json));
     reply(obj.facts[0]);
   }
 }
