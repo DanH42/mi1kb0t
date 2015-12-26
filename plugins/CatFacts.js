5250e4bc173e7db848853345c46931f6a3af1a45
twof <fabiobean2@gmail.com>
2015-12-25 16:04:59 -0800

    made reccomended changes to plugin

--- plugins/CatFacts.js
+++ plugins/CatFacts.js
@@ -1,16 +1,17 @@
+var request = require('request');
+
 module.exports = {listeners: [
 {
-	type: "equals",
-	query: "CF?",
-  function Get(yourUrl){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-  }
-  callback: function(reply, message){
-    var json = "http://catfacts-api.appspot.com/api/facts?number=1";
-    obj = JSON.parse(Get(json));
-    reply(obj.facts[0]);
-  }
+    type: "equals",
+    query: ".cat",
+    callback: function(reply, message){
+        request("http://catfacts-api.appspot.com/api/facts?number=1", function(err, res, body){
+            try{
+                var obj = JSON.parse(body);
+                if(obj && obj.facts && obj.facts[0])
+                    reply(obj.facts[0]);
+            }catch(e){}
+        });
+    }
 }
+]};
