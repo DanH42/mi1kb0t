8522df52496fe7439bcc391809278158b41cb1b7
Dan Hlavenka <dan@hlavenka.me>
2016-01-02 22:09:42 -0600

    Added optional "3d20" style notation for dice rolls

--- plugins/random.js
+++ plugins/random.js
@@ -1,22 +1,51 @@
-// Various assorted call/response combinations
+// Get some random numbers
 
 module.exports = {listeners: [
 {
     type: "startsWith",
     query: ".dice",
-    callback: function(reply, message){
-        var words = message.body.match(/[0-9]+/);
-        if (words && words[0] && !isNaN(words[0])) {
-            reply(Math.floor(Math.random() * words[0]) + 1 + "");
-            return;
-        }
-        reply(Math.floor(Math.random() * 6) + 1 + "");
-    }
+    callback: roll
+}, {
+    type: "startsWith",
+    query: ".roll",
+    callback: roll
 }, {
     type: "equals",
     query: ".coin",
     callback: function(reply){
-        reply(Math.random()<0.5 ? "Heads" : "Tails");
+        reply(Math.random() < 0.5 ? "Heads" : "Tails");
     }
 }
 ]};
+
+function roll(reply, message){
+	var sides = 6;
+	var rolls = 1;
+	var overflow = false;
+
+	var match = message.body.match(/(?:([0-9]*)d)?([0-9]+)/i);
+	console.log(match);
+	if(match && match.length === 3){
+		if(match[1])
+			rolls = parseInt(match[1])
+		sides = parseInt(match[2]);
+
+		if(rolls > 10){
+			rolls = 10;
+			overflow = true;
+		}
+	}
+
+	var rolled = 0;
+	var nextRoll = function(){
+		reply(Math.floor(Math.random() * sides) + 1 + "");
+
+		if(++rolled < rolls)
+			setTimeout(nextRoll, 100);
+		else if(overflow)
+			setTimeout(function(){
+				reply("Ran out of dice!");
+			}, 100);
+	}
+	nextRoll();
+}
