525683db16750705d98f939d2fd52f588beb36d4
Dan Hlavenka <dan@hlavenka.me>
2016-01-08 12:02:47 -0600

    Stop responding with "That's me!"

--- plugins/read.js
+++ plugins/read.js
@@ -1,17 +1,13 @@
 // Mark all messages as read in threads where your name was recently mentioned
-// On non-Messenger networks, respond with "That's me!" when first mentioned.
 
 module.exports = {listeners: [
 {
 	type: "all",
 	callback: function(reply, message, api){
-		if(message.isAddressed === 2){
-			if(api.type === "messenger")
+		if(api.type === "messenger"){
+			if(message.isAddressed === 2)
 				api.markAsRead(message.thread_id);
-			else
-				reply("That's me!");
-		}else if(message.isAddressed === 1){
-			if(api.type === "messenger")
+			else if(message.isAddressed === 1)
 				api.markAsRead(message.thread_id);
 		}
 	}
