02a42a118aaa8ab15bc3c14b2acf75657bddd188
Dan Hlavenka <dan@hlavenka.me>
2018-01-09 13:28:17 -0600

    Numbers

--- plugins/pingpong.js
+++ plugins/pingpong.js
@@ -59,7 +59,7 @@ module.exports = {listeners: [
 	type: "regex",
 	query: /(?:(?:^| )is|'s|'re) (.*?)(?: and| or| but|http|[,.?!:()]|$)/i,
 	callback: function(reply, message, api, match){
-		// Always respond if recently mentioned, otherwise 10% chance
+		// Always respond if recently mentioned, otherwise 5% chance
 		if((message.isAddressed || Math.random() < 0.05)){
 			var comeback = match[1].trim();
 			// If they're shouting, shout back
