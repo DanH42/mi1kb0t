c1fd3493fa92656b73d73dd16b75499dafd9d350
Dan Hlavenka <dan@hlavenka.me>
2018-03-15 22:35:20 -0500

    Switched Cat Facts over to catfact.ninja

--- plugins/CatFacts.js
+++ plugins/CatFacts.js
@@ -5,12 +5,14 @@ module.exports = {listeners: [
     type: "equals",
     query: ".cat",
     callback: function(reply, message){
-        request("http://catfacts-api.appspot.com/api/facts?number=1", function(err, res, body){
+        request("http://catfact.ninja/fact", function(err, res, body){
             try{
                 var obj = JSON.parse(body);
-                if(obj && obj.facts && obj.facts[0])
-                    reply(obj.facts[0]);
+                if(obj && obj.fact)
+                    return reply(obj.fact);
             }catch(e){}
+
+			reply("Sorry, no cat facts for you today :'(");
         });
     }
 }
