20bf832eeefcd96de70dba765c3cffa8e651ce9e
Dan Hlavenka <dan@hlavenka.me>
2018-03-17 01:41:30 -0500

    Possums!

--- plugins/reddit.js
+++ plugins/reddit.js
@@ -19,22 +19,28 @@ module.exports = {listeners: [
 	callback: function(reply, message, api){
 		reddit(["doge", "supershibe"], "http://i.imgur.com/WMiliXD.jpg", reply, message, api);
 	}
+}, {
+	type: "regex",
+	query: /.o?pos+um/i,
+	callback: function(reply, message, api){
+		reddit(["Possums"], "https://i.imgur.com/T0wMPRg.jpg", reply, message, api);
+	}
 }
 ]};
 
 function reddit(subs, fail, reply, message, api){
 	if(api.type === "messenger")
 		api.sendTypingIndicator(message.thread_id);
-	var url = "https://www.reddit.com/r/" + subs[Math.floor(Math.random() * subs.length)] + "/top/.json?sort=top&t=week";
+	var url = "https://www.reddit.com/r/" + subs[Math.floor(Math.random() * subs.length)] + "/top/.json?sort=top&t=year";
 
 	request(url, function(err, res, body){
 		try{
 			var posts = JSON.parse(body).data.children;
 			var index = Math.floor(Math.random() * posts.length);
 			var attempts = 0;
-			while(!posts[index].data.url || posts[index].data.url.indexOf("i.imgur.com") === -1){
+			while(!posts[index].data.url || (posts[index].data.url.indexOf("i.imgur.com") === -1 && posts[index].data.url.indexOf("i.redditmedia.com") === -1)){
 				index = Math.floor(Math.random() * posts.length);
-				if(++attempts > 10)
+				if(++attempts > 50)
 					return reply({attachment: request(fail)});
 			}
 			var post = posts[index].data.url;
