88fc1fbcc9f185dae2a08069b90d44e1b835f798
Dan Hlavenka <dan@hlavenka.me>
2018-05-07 10:17:11 -0500

    Move "no u" behavior to its own file

---
+++ plugins/nou.js
@@ -0,0 +1,25 @@
+// Turn messages back around on their sender
+
+var responseTimes = {};
+module.exports = {listeners: [
+{
+	type: "regex",
+	query: /(?:(?:^| )is|(?:'|’)s|(?:'|’)re) (.*?)(?: and| or| but|http|[,.?!:()]|$)/i,
+	callback: function(reply, message, api, match){
+		// Always respond if recently mentioned, otherwise 5% chance
+		if((message.isAddressed || Math.random() < 0.05)){
+			var time = new Date().getTime();
+			// Don't respond unless it's been at least half an hour, or the current message is also a mention
+			if(message.isAddressed === 2 || !responseTimes[message.threadID] || time - responseTimes[message.threadID] > 1800000){
+				responseTimes[message.threadID] = time;
+				var comeback = match[1].trim();
+				// If they're shouting, shout back
+				if(comeback.toUpperCase() === comeback && comeback.toLowerCase() !== comeback)
+					reply("YOUR MOM'S " + comeback + "!!!");
+				else
+					reply("YOU'RE " + comeback + ".");
+			}
+		}
+	}
+}
+]};
--- plugins/pingpong.js
+++ plugins/pingpong.js
@@ -64,20 +64,6 @@ module.exports = {listeners: [
 		reply(insults[Math.floor(Math.random() * insults.length)]);
 	}
 }, {
-	type: "regex",
-	query: /(?:(?:^| )is|'s|'re) (.*?)(?: and| or| but|http|[,.?!:()]|$)/i,
-	callback: function(reply, message, api, match){
-		// Always respond if recently mentioned, otherwise 5% chance
-		if((message.isAddressed || Math.random() < 0.05)){
-			var comeback = match[1].trim();
-			// If they're shouting, shout back
-			if(comeback.toUpperCase() === comeback && comeback.toLowerCase() !== comeback)
-				reply("YOUR MOM'S " + comeback + "!!!");
-			else
-				reply("YOU'RE " + comeback + ".");
-		}
-	}
-}, {
 	type: "contains",
 	query: "thank",
 	callback: function(reply, message){
